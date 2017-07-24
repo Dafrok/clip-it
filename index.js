44a4744f8fc30b3594ad2ccf26be7c161e3962f6
Dafrok <o.o@mug.dog>
2017-07-24 16:36:44 +0800

    Optimize for iOS

--- index.js
+++ index.js
@@ -7,21 +7,25 @@
   var index = (function (str) {
     var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
 
-    var $input = document.createElement('input');
+    var $input = document.createElement('textarea');
+    $input.setAttribute('readonly', '');
+    $input.style.fontSize = '12pt';
+    $input.style.width = '0';
+    $input.style.height = '0';
+    $input.style.opacity = '0';
+    $input.style.top = '-1000px';
+    $input.style.position = 'absolute';
     document.body.appendChild($input);
-    $input.addEventListener(function (e) {
+    $input.addEventListener('copy', function (e) {
       e.preventDefault();
       e.clipboardData.setData(options.contentType || 'text/plain', e.target.value);
     });
     $input.value = str;
-    $input.focus();
     $input.selectStart = 0;
     $input.selectEnd = $input.value.length;
     $input.select();
-    setTimeout(function () {
-      document.execCommand('copy');
-      document.body.removeChild($input);
-    });
+    document.execCommand('copy');
+    document.body.removeChild($input);
   });
 
   return index;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
 export default (str, options = {}) => {
-  const $input = document.createElement('input');
+  const $input = document.createElement('textarea');
+  $input.setAttribute('readonly', '')
+  $input.style.fontSize = '12pt';
   $input.style.width = '0'
   $input.style.height = '0'
   $input.style.opacity = '0'
-  $input.style.top = '-9999px'
-  $input.style.position = 'fixed'
+  $input.style.top = '-1000px'
+  $input.style.position = 'absolute'
   document.body.appendChild($input)
   $input.addEventListener('copy', e => {
     e.preventDefault()
     e.clipboardData.setData(options.contentType || 'text/plain', e.target.value)
   })
   $input.value = str
-  $input.focus()
   $input.selectStart = 0
   $input.selectEnd = $input.value.length
   $input.select()
-  setTimeout(() => {
-    document.execCommand('copy')
-    document.body.removeChild($input)
-  })
+  document.execCommand('copy')
+  document.body.removeChild($input)
 }
