b1c9c43c05aa02dec7c0474a5e3784b45a779e06
马金花儿 <o.o@mug.dog>
2017-07-24 15:49:21 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-export default str => {
+const 
+
+export default (str, options) => {
   const $input = document.createElement('input');
   document.body.appendChild($input)
+  $input.addEventListener(e => {
+    e.preventDefault()
+    e.clipboardData.setData(options.contentType || 'text/plain', e.target.value)
+  })
   $input.value = str
   $input.focus()
   $input.selectStart = 0
   $input.selectEnd = $input.value.length
   $input.select()
-  document.body.clientWidth
-  document.execCommand('copy')
-  document.body.removeChild($input)
+  setTimeout(setTimeout => {
+    document.execCommand('copy')
+    document.body.removeChild($input)
+  })
 }
