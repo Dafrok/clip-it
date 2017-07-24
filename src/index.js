469258cf446a341419a78ba043a9bc50c60b1f6a
Dafrok <o.o@mug.dog>
2017-07-24 16:20:49 +0800

    Bug fixed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 export default (str, options = {}) => {
   const $input = document.createElement('input');
+  $input.style.width = '0'
+  $input.style.height = '0'
+  $input.style.opacity = '0'
+  $input.style.top = '-9999px'
+  $input.style.position = 'fixed'
   document.body.appendChild($input)
-  $input.addEventListener(e => {
+  $input.addEventListener('copy', e => {
     e.preventDefault()
     e.clipboardData.setData(options.contentType || 'text/plain', e.target.value)
   })
