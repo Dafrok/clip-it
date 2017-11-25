f6bd49a3f81e25e9446b1fd2b0a544e8c97882ed
Dafrok <o.o@mug.dog>
2017-11-26 02:03:33 +0800

    build

--- index.js
+++ index.js
@@ -4,29 +4,59 @@
   (global.clipIt = factory());
 }(this, function () { 'use strict';
 
-  var index = (function (str) {
+  var index = (function (value) {
     var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
 
-    var $input = document.createElement('textarea');
-    $input.setAttribute('readonly', '');
-    $input.style.fontSize = '12pt';
-    $input.style.width = '0';
-    $input.style.height = '0';
-    $input.style.opacity = '0';
-    $input.style.top = '-1000px';
-    $input.style.position = 'absolute';
-    document.body.appendChild($input);
-    $input.addEventListener('copy', function (e) {
-      e.preventDefault();
-      e.clipboardData.setData(options.contentType || 'text/plain', e.target.value);
-    });
-    $input.value = str;
-    $input.selectStart = 0;
-    $input.selectEnd = $input.value.length;
-    $input.select();
-    document.execCommand('copy');
-    document.body.removeChild($input);
+    return copyByInput(value, options) || copyByContentEditable(value, options);
   });
+
+  var insertInvisibleEl = function insertInvisibleEl($el) {
+    $el.style.zoom = '0';
+    $el.style.width = '0';
+    $el.style.height = '0';
+    $el.style.opacity = '0';
+    $el.style.pointerEvents = 'none';
+    $el.style.top = '-1000px';
+    $el.style.position = 'absolute';
+    document.body.appendChild($el);
+  };
+
+  var excuteCopy = function excuteCopy($el, value, options) {
+    $el.addEventListener('copy', function (e) {
+      return e.clipboardData.setData(options.contentType || 'text/plain', value);
+    });
+    var result = document.execCommand('copy');
+    document.body.removeChild($el);
+    return result;
+  };
+
+  var copyByContentEditable = function copyByContentEditable(value, options) {
+    var $el = document.createElement('span');
+    $el.innerHTML = value;
+    $el.contentEditable = true;
+    insertInvisibleEl($el);
+
+    var range = document.createRange();
+    range.selectNodeContents($el);
+    var selection = window.getSelection();
+    selection.removeAllRanges();
+    selection.addRange(range);
+
+    return excuteCopy($el, value, options);
+  };
+
+  var copyByInput = function copyByInput(value, options) {
+    var $el = document.createElement('textarea');
+    $el.setAttribute('readonly', '');
+    insertInvisibleEl($el);
+
+    $el.value = value;
+    $el.selectStart = 0;
+    $el.selectEnd = $input.value.length;
+    $el.select();
+
+    return excuteCopy($el, value, options);
+  };
 
   return index;
 
