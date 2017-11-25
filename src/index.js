dfea14c7e8035e2a5dd5cac712d4fca11f423421
Dafrok <o.o@mug.dog>
2017-11-25 11:17:54 +0800

    Add copy handler by contenteditable elements

--- src/index.js
+++ src/index.js
@@ -1,21 +1,49 @@
-export default (str, options = {}) => {
-  const $input = document.createElement('textarea');
-  $input.setAttribute('readonly', '')
-  $input.style.fontSize = '12pt';
-  $input.style.width = '0'
-  $input.style.height = '0'
-  $input.style.opacity = '0'
-  $input.style.top = '-1000px'
-  $input.style.position = 'absolute'
-  document.body.appendChild($input)
-  $input.addEventListener('copy', e => {
-    e.preventDefault()
-    e.clipboardData.setData(options.contentType || 'text/plain', e.target.value)
-  })
-  $input.value = str
-  $input.selectStart = 0
-  $input.selectEnd = $input.value.length
-  $input.select()
-  document.execCommand('copy')
-  document.body.removeChild($input)
+export default (value, options = {}) => {
+  return copyByInput(value, options) || copyByContentEditable(value, options)
+}
+
+const insertInvisibleEl = $el => {
+  $el.style.zoom = '0'
+  $el.style.width = '0'
+  $el.style.height = '0'
+  $el.style.opacity = '0'
+  $el.style.pointerEvents = 'none'
+  $el.style.top = '-1000px'
+  $el.style.position = 'absolute'
+  document.body.appendChild($el)
+}
+
+const excuteCopy = ($el, value, options) => {
+  $el.addEventListener('copy', e => e.clipboardData.setData(options.contentType || 'text/plain', value))
+  const result = document.execCommand('copy')
+  document.body.removeChild($el)
+  return result
+}
+
+const copyByContentEditable = (value, options) => {
+  const $el = document.createElement('span')
+  $el.innerHTML = value
+  $el.contentEditable = true
+  insertInvisibleEl($el);
+
+  const range = document.createRange()
+  range.selectNodeContents($el)
+  const selection = window.getSelection()
+  selection.removeAllRanges()
+  selection.addRange(range)
+
+  return excuteCopy($el, value, options)
+}
+
+const copyByInput = (value, options) => {
+  const $el = document.createElement('textarea')
+  $el.setAttribute('readonly', '')
+  insertInvisibleEl($el)
+
+  $el.value = value
+  $el.selectStart = 0
+  $el.selectEnd = $input.value.length
+  $el.select()
+
+  return excuteCopy($el, value, options)
 }
