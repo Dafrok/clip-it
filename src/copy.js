8935e84f088065472ef33deeac149cca397c169a
Dafrok <o.o@mug.dog>
2017-12-30 12:00:00 +0800

    init doc

--- src/copy.js
+++
@@ -1,7 +0,0 @@
-export default ($el, value, options) => {
-  /* istanbul ignore next */
-  $el.addEventListener('copy', e => e.clipboardData.setData(options.contentType || 'text/plain', value))
-  const result = document.execCommand('copy')
-  document.body.removeChild($el)
-  return result
-}
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import copyByContentEditable from './contenteditable.js'
-import copyByInput from './input.js'
-
-export default (value, options = {}) =>
-  /* istanbul ignore next */
-  copyByInput(value, options) || copyByContentEditable(value, options)
-
