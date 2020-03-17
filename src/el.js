d05e45862c6db62b91389f208dbea66e0fc61f2a
Dafrok <o.o@mug.dog>
2020-03-17 15:14:37 +0800

    compatible new chrome

--- src/el.js
+++ src/el.js
@@ -1,11 +1,9 @@
 export default tag => {
   const $el = document.createElement(tag)
-  $el.style.zoom = '0'
-  $el.style.width = '0'
-  $el.style.height = '0'
-  $el.style.opacity = '0'
   $el.style.pointerEvents = 'none'
   $el.style.top = '-1000px'
+  $el.style.left = '-1000px'
+  $el.transform = 'scale(0)'
   $el.style.position = 'absolute'
   document.body.appendChild($el)
   return $el
